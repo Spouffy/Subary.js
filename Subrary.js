50209047a5e9c84ceeeeec31833cb9318dd6c84d
Spouffy <65297560+Spouffy@users.noreply.github.com>
2021-04-19 11:41:20 +0200

    Added createElm(tag, id, parent_id) funciton

--- Subrary.js
+++ Subrary.js
@@ -1,6 +1,8 @@
 //Spouffy's library
 // Open source
 
+function l(v){console.log(v)}
+
 function hide(e){
     try {
         document.getElementById(e).style.display = 'none'
@@ -56,3 +58,44 @@ function ElmFollowCursor(delay, id){
     }else{
         console.error('imgFollowCursor : please, provide an integer and then a string. EX: ElmFollowCursor(100, "imgId")');
 }}
+
+function pgcd(min, max){ // PGCD calculator function - TO DO !
+    max = Math.max(min,max);
+    min = Math.min(min,max);
+    var temp;
+    var state = false;
+    var bigger;
+    var smaller;
+    if (min > max || min < max){
+            state = false;
+            temp = max - min;
+            bigger = Math.max(temp, min);
+            smaller = Math.min(temp, min);
+        while(!state){
+                temp = bigger - smaller;
+                bigger = Math.max(temp, smaller);
+                smaller = Math.min(temp, smaller);
+            if(temp == 0){
+                state = true;
+                return bigger;
+            }
+    }
+    }
+}
+
+function randomInt(min, max){
+    rand = Math.round(Math.random() * (max - min) ) + min;
+    return rand
+}
+
+function createElm(tag, id, parent_id){
+    try {
+        var parent = document.getElementById(parent_id)
+        var e = document.createElement(tag);
+        parent.appendChild(e);
+        e.id = id;
+        return e;
+    } catch (error) {
+        l(error)
+    }
+}
